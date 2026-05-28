76f4f91320c1206e0e5883dbd719528dade7e626
YatiMaheshwari26 <yatimaheshwari5165@gmail.com>
2026-05-28 15:59:55 +0530

    day1

---
+++ day1/src/SumNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum=sum+i;
+        }
+        System.out.println("The sum is "+sum);
+        sc.close();
+
+    }
+}
