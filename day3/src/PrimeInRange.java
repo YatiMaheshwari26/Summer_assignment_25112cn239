ca30441fa45c682c12f958f7712ecfa278315abd
YatiMaheshwari26 <yatimaheshwari5165@gmail.com>
2026-05-29 21:40:41 +0530

    day3

---
+++ day3/src/PrimeInRange.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PrimeInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+          System.out.print("Enter starting number: ");
+          int start = sc.nextInt();
+          System.out.print("Enter ending number: ");
+          int end = sc.nextInt();
+          System.out.println("Prime numbers between " + start + " and " + end);
+          for (int num = start; num <= end; num++){
+
+        if (num<=1) {
+            continue;
+        }
+        boolean isPrime= true;
+            for (int i=2; i<=num/2; i++) {
+                if (num %i ==0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+        if (isPrime) {
+            System.out.println( num + " Prime number");
+           }
+        }
+        sc.close();
+    }
+}
+
