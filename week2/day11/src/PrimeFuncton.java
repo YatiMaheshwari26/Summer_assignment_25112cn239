fef451d573e7574af52e2dcf13705fb500bb691f
YatiMaheshwari26 <yatimaheshwari5165@gmail.com>
2026-06-11 13:53:33 +0530

    day11

---
+++ week2/day11/src/PrimeFuncton.java
@@ -0,0 +1,22 @@
+public class PrimeFuncton {
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int num = 17;
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number");
+        }
+        else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
