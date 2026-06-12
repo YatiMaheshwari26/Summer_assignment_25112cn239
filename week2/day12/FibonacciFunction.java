83951e43774c0660188d554eec51ac30b677929e
YatiMaheshwari26 <yatimaheshwari5165@gmail.com>
2026-06-12 12:32:19 +0530

    day12

---
+++ week2/day12/FibonacciFunction.java
@@ -0,0 +1,15 @@
+public class FibonacciFunction {
+    static void fibonacci(int n) {
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a+ " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        fibonacci(n);
+    }
+}
