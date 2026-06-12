adb94aebe043a215d9b497e9ab88b1e68f980b7e
YatiMaheshwari26 <yatimaheshwari5165@gmail.com>
2026-06-12 12:31:23 +0530

    day12

---
+++ week2/day12/PalindromeFunction.java
@@ -0,0 +1,20 @@
+public class PalindromeFunction {
+    static boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+        return original == reversed;
+    }
+    public static void main(String[] args) {
+        int num = 121;
+        if (isPalindrome(num)) {
+            System.out.println(num+" is a palindrome.");
+        } else  {
+            System.out.println(num+" is not a palindrome.");
+        }
+    }
+}
