36d05506f49fafeb7d280e6512a5eaacacdf42d0
Yati Maheshwari <yatimaheshwari5165@gmail.com>
2026-06-21 21:21:30 +0530

    Add files via upload

---
+++ week3/day21/StringLengthWithoutLength.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringLengthWithoutLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+            System.out.println("Length of string = " + count);
+        }
+
+        sc.close();
+    }
+}
